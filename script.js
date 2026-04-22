b7eb02727e92c27756c369db470bf347580eecb9
Matias Fauda <matiasfauda@gmail.com>
2026-04-22 15:30:53 -0300

    fix: corrijo e implemento mostrarDetalle de forma tradicional

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const mostrarDetalles = (nombreProyecto) => {
     const mensaje = `Has seleccionado el proyecto: ${nombreProyecto}`;
     alert(mensaje);
 };
+
+// Declaracion basica
+function mostrarDetalles(nombreProyecto) {
+    const mensaje = `Has seleccionado el proyecto: ${nombreProyecto}`;
+    alert(mensaje);
+}
